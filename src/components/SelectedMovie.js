4613f9b81c7724a8965cf23701b6aff85d9f7b37
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-19 21:48:16 -0500

    change my website url

--- src/components/SelectedMovie.js
+++ src/components/SelectedMovie.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { useMyCatalog } from '../Contexts/MyCatalogProvider'
-import ReactPlayer from 'react-player'
-
+import React from "react";
+import { useMyCatalog } from "../Contexts/MyCatalogProvider";
+import ReactPlayer from "react-player";
 
 export default function SelectedMovie() {
-    const { movies, selectedMovie } = useMyCatalog()
-    const selectedMovieInfo = selectedMovie
+  const { movies, selectedMovie } = useMyCatalog();
+  const selectedMovieInfo = selectedMovie;
   return (
-    <div style={{color: 'white'}}>
+    <div style={{ color: "white" }}>
       <h1>{selectedMovieInfo?.title}</h1>
-      <div className='selectedMovie--movie' id='selectedMovie'>
-        <img src={selectedMovieInfo?.poster}/>
-        <ReactPlayer url={selectedMovieInfo?.trailer} controls={true} className='selectedMovie--video'/>
+      <button className="app--website-button" onClick={goToMyWebsite}>
+        Go To My Website
+      </button>
+      <div className="selectedMovie--movie" id="selectedMovie">
+        <img src={selectedMovieInfo?.poster} />
+        <ReactPlayer
+          url={selectedMovieInfo?.trailer}
+          controls={true}
+          className="selectedMovie--video"
+        />
       </div>
-      <h3 className='selectedMovie--plot'>{selectedMovieInfo?.plot}</h3>
+      <h3 className="selectedMovie--plot">{selectedMovieInfo?.plot}</h3>
     </div>
-  )
+  );
+
+  function goToMyWebsite() {
+    window.open("https://marcelobrasil.ca/");
+  }
 }
